602ac8bd59e44f375dd6cf92936b055f264cffc5
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-01-13 23:50:52 +0900

    Title: 훌륭한 점프, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

--- 260113/훌륭한 점프/great-jump.py
+++ 260113/훌륭한 점프/great-jump.py
@@ -1,17 +1,22 @@
+import sys
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
 max_val = max(arr)
 
-ans=0
+ans=sys.maxsize
 def its_possible(i):
     indexes=list()
     for j, elem in enumerate(arr):
         if j==0 or j==n-1 or elem<=i:
             indexes.append(j)
     
-    if max(indexes)!=i:
+    max_val2=0
+    for j in indexes:
+        max_val2 = max(arr[j], max_val2)
+    
+    if max_val2!=i:
         return False
 
     before_index=0
@@ -26,6 +31,7 @@ def its_possible(i):
 
 for i in range(1, max_val+1):
     if its_possible(i):
-        ans = max(ans, i)
+        # print(i)
+        ans = min(ans, i)
 
 print(ans)
