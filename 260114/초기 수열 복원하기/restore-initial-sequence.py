b98a8a524ab6af3aecbfa0aef3c0a1f9a2151ee1
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-01-14 22:43:40 +0900

    Title: 초기 수열 복원하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260114/초기 수열 복원하기/restore-initial-sequence.py
@@ -0,0 +1,17 @@
+n = int(input())
+adjacent = list(map(int, input().split()))
+
+# Please write your code here.
+
+for i in range(1, n+1):
+    arr=list()
+    arr.append(i)
+    for j in range(n-1):
+        arr.append(abs(adjacent[j]-arr[j]))
+    arr = set(arr)
+    if len(arr)==n:
+        break
+
+
+for i in arr:
+    print(i, end=' ')
