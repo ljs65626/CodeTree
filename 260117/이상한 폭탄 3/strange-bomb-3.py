3ab0a5967abd2ead48cdc5717c9377733bb64404
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-01-17 22:49:04 +0900

    Title: 이상한 폭탄 3, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260117/이상한 폭탄 3/strange-bomb-3.py
+++ 260117/이상한 폭탄 3/strange-bomb-3.py
@@ -1,21 +1,49 @@
 n, k = map(int, input().split())
 num = [int(input()) for _ in range(n)]
-
+max_a = max(num)
 # Please write your code here.
-ans=0
-max_val=0
+# ans=0
+# max_val=0
+# for i in range(n):
+#     cnt=0
+#     for j in range(i+1, i+k+1):
+#         if j>=n:
+#             break
+#         if num[i]==num[j]:
+#             if cnt==0:
+#                 cnt+=1
+#             cnt+=1
+#     if cnt>=1 and cnt>=max_val:
+#         max_val=cnt
+#         if num[i]>ans:
+#             ans=num[i]
+
+# print(ans)
+# print(max_val)
+
+bomb = [0] * (max_a+1)
+exploded = [False] * n
 for i in range(n):
-    cnt=0
-    for j in range(i, i+k+1):
+    for j in range(i+1, i+k+1):
         if j>=n:
             break
-        if i!=j and num[i]==num[j]:
-            if cnt==0:
-                cnt+=1
-            cnt+=1
-    if cnt>=1 and cnt>=max_val:
-        max_val=cnt
-        if num[i]>ans:
-            ans=num[i]
+        if num[i]!=num[j]:
+            continue
+        if exploded[i]==False:
+            bomb[num[i]]+=1
+            exploded[i]=True
+        if exploded[j]==False:
+            bomb[num[j]]+=1
+            exploded[j]=True
 
-print(ans)
+maxval=1
+maxidx=0
+for i in range(1, max_a+1):
+    if bomb[i]>=maxval:
+        maxval=bomb[i]
+        maxidx=i
+
+print(maxidx)
+# print(maxval)
+
+
