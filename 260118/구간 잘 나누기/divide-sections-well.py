f21b48cf358534aa7d362db04d2b7e8a8dac6768
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-01-18 18:15:48 +0900

    Title: 구간 잘 나누기, Time: 54ms, Memory: 17MB, Status: Passed - Codetree

--- 260118/구간 잘 나누기/divide-sections-well.py
+++ 260118/구간 잘 나누기/divide-sections-well.py
@@ -2,19 +2,19 @@ n, m = map(int, input().split())
 a = list(map(int, input().split()))
 
 # Please write your code here.
-
-for i in range(100001):
+maxa=max(a)
+for i in range(maxa, 100001):
     sumval=0
     cnt=0
     for j in a:
         if sumval+j<=i:
             sumval+=j
-        else:
+        elif sumval+j>i:
             cnt+=1
             sumval=j
     cnt+=1
 
-    if cnt==m:
+    if cnt<=m:
         print(i)
         break
     
