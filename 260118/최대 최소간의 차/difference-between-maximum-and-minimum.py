1810f286fb0b5329711fade8bf3d0660c12e0c9b
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-01-18 20:43:44 +0900

    Title: 최대 최소간의 차, Time: 69ms, Memory: 18MB, Status: Passed - Codetree

--- 260118/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 260118/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -3,18 +3,20 @@ n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
+def get_cost(low, high):
+    cost=0
+    for i in range(n):
+        if arr[i]>high:
+            cost+=(arr[i]-high)
+        elif arr[i]<low:
+            cost+=(low-arr[i])
+    
+    return cost
+
 
 ans=sys.maxsize
 for mini in range(1, 10001):
-    maxi = k+mini
-    if maxi>10000:
-        break
-    cost=0
-    for i in range(n):
-        if arr[i]>maxi:
-            cost+=(arr[i]-maxi)
-        elif arr[i]<mini:
-            cost+=(mini-arr[i])
+    cost = get_cost(mini, mini+k)
     
     ans = min(cost, ans)
 
