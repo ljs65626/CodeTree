5df12a08c2def56fcf4a9945dd0ecdaa824d02c6
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-01-25 20:44:23 +0900

    Title: 두 선분, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260125/두 선분/two-lines.py
+++ 260125/두 선분/two-lines.py
@@ -2,21 +2,26 @@ x1, x2, x3, x4 = map(int, input().split())
 
 # Please write your code here.
 
-arr = [0] * 101
+# arr = [0] * 101
 
-for i in range(x1, x2+1):
-    arr[i] += 1
+# for i in range(x1, x2+1):
+#     arr[i] += 1
 
-for i in range(x3, x4+1):
-    arr[i] += 1
+# for i in range(x3, x4+1):
+#     arr[i] += 1
 
-is_satisfied=True
-for i in range(1, 101):
-    if arr[i]>1:
-        is_satisfied=False
-        break
+# is_satisfied=True
+# for i in range(1, 101):
+#     if arr[i]>1:
+#         is_satisfied=False
+#         break
 
-if is_satisfied:
+# if is_satisfied:
+#     print('nonintersecting')
+# else:
+#     print('intersecting')
+
+if x2<x3 or x4<x1:
     print('nonintersecting')
 else:
     print('intersecting')
