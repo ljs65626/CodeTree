268fb345ed473cc14639ceb6f6e8bd0dffc432c3
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-01-25 22:24:11 +0900

    Title: 전부 겹치는 선분, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260125/전부 겹치는 선분/overlapping-line-segments.py
+++ 260125/전부 겹치는 선분/overlapping-line-segments.py
@@ -3,16 +3,28 @@ n = int(input())
 segments = [tuple(map(int, input().split())) for _ in range(n)]
 
 
-# Please write your code here.
-arr = [0] * 101
+# # Please write your code here.
+# arr = [0] * 101
 
+# for x, y in segments:
+#     for i in range(x, y+1):
+#         arr[i] += 1
+
+# for i in range(1, 101):
+#     if arr[i]==n:
+#         print('Yes')
+#         sys.exit()
+
+# print('No')
+
+maxx = -sys.maxsize
+miny = sys.maxsize
 for x, y in segments:
-    for i in range(x, y+1):
-        arr[i] += 1
+    maxx = max(maxx, x)
+    miny = min(miny, y)
 
-for i in range(1, 101):
-    if arr[i]==n:
-        print('Yes')
-        sys.exit()
 
-print('No')
+if maxx<=miny:
+    print('Yes')
+else:
+    print('No')
