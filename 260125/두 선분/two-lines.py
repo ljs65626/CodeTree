5626b52bb3cbda212ff45c2faafaf38aacb77cb5
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-01-25 20:37:06 +0900

    Title: 두 선분, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260125/두 선분/two-lines.py
@@ -0,0 +1,22 @@
+x1, x2, x3, x4 = map(int, input().split())
+
+# Please write your code here.
+
+arr = [0] * 101
+
+for i in range(x1, x2+1):
+    arr[i] += 1
+
+for i in range(x3, x4+1):
+    arr[i] += 1
+
+is_satisfied=True
+for i in range(1, 101):
+    if arr[i]>1:
+        is_satisfied=False
+        break
+
+if is_satisfied:
+    print('nonintersecting')
+else:
+    print('intersecting')
