16b3dc147507fe88e94e609256874942f1992f1f
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-01-26 12:12:30 +0900

    Title: 비둘기와 전기줄, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260126/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 260126/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -3,18 +3,17 @@ N = int(input())
 
 
 # Please write your code here.
-is_First = [True] * 11
-number_pos = [0] * 11
+is_First = [-1] * 11
 
 ans=0
 for _ in range(N):
     p, pos = map(int, input().split())
-    if is_First[p]==True:
-        is_First[p]=False
-        number_pos[p] = pos
+    if is_First[p]==-1:
+        is_First[p] = pos
     else:
-        if number_pos[p]!=pos:
+        if is_First[p]!=pos:
             ans+=1
-            number_pos[p] = pos
+            is_First[p] = pos
 
 print(ans)
+
