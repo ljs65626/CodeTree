9ddbf93f4b5a9822ef98cc414c42f9d9a203fa13
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-01-30 20:04:31 +0900

    Title: 코딩톡, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260130/코딩톡/coding-talk.py
+++ 260130/코딩톡/coding-talk.py
@@ -1,6 +1,8 @@
+import sys
 n, m, p = map(int, input().split())
 message = [tuple(input().split()) for _ in range(m)]
 # Please write your code here.
+
 start=65
 programers = []
 for _ in range(n):
@@ -11,10 +13,14 @@ is_read = [False] * n
 for i, (c, u) in enumerate(message, start=1):
     if i<p:
         continue
+    u = int(u)
+    if p==i and u==0:
+        sys.exit()
     is_read[programers.index(c)] = True
 
 
 for i in range(n):
     if is_read[i]==False:
         print(programers[i], end=' ')
+
 
