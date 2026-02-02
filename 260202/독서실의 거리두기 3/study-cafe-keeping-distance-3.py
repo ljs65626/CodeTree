c13da9a95cb5afb996eb8b5b2a9b4db6aac660c1
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-02-02 21:30:49 +0900

    Title: 독서실의 거리두기 3, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260202/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,31 @@
+import sys
+N = int(input())
+seats = list(input())
+
+# Please write your code here.
+
+maxdiff = -sys.maxsize
+ans = -sys.maxsize
+
+
+before=0
+for i in range(1, N):
+    if seats[i]=='1':
+        diff = i-before
+
+        if diff>maxdiff:
+            maxdiff = diff
+            maxfirst = before
+            maxlast = i
+
+        before = i
+ii = (maxfirst+maxlast)//2
+seats[ii] = '1'
+
+for i in range(1,N):
+    if seats[i]=='1':
+        diff = i-before
+        before = i
+        ans = max(ans, diff)
+
+print(ans)
