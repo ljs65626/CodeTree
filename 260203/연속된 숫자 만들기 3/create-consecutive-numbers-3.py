fd487aab8b9de2a684cd892ac70a143511915d56
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-02-03 17:31:26 +0900

    Title: 연속된 숫자 만들기 3, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260203/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 260203/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,6 +1,11 @@
+import sys
 a = list(map(int, input().split()))
 
 # Please write your code here.
+
+if (a[1]-a[0]==1) and (a[2]-a[1]==1):
+    print(0)
+    sys.exit()
 
 if a[1]-a[0]>a[2]-a[1]:
     print(a[1]-a[0]-1)
