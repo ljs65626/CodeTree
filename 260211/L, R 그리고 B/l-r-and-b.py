2c0beb2583fa5b115a4f5ac9cbb6eb216152088c
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-02-11 10:44:32 +0900

    Title: L, R 그리고 B, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260211/L, R 그리고 B/l-r-and-b.py
+++ 260211/L, R 그리고 B/l-r-and-b.py
@@ -9,5 +9,11 @@ for i in range(10):
         elif board[i][j]=='L':
             li=i
             lj=j
+        elif board[i][j]=='R':
+            ri=i
+            rj=j
 
-print(abs(bi-li)+abs(bj-lj)-1)
+if bi==li==ri or bj==lj==rj:
+    print((abs(bi-li)+abs(bj-lj)-1)+2)
+else:
+    print(abs(bi-li)+abs(bj-lj)-1)
