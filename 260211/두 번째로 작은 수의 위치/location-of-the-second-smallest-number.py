10caaaffaf34851f486d0c6122fb4ea8716ff35e
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-02-11 10:26:12 +0900

    Title: 두 번째로 작은 수의 위치, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260211/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
+++ 260211/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -1,16 +1,19 @@
 import sys
 n = int(input())
 a = list(map(int, input().split()))
-second_min = sys.maxsize
-second_index=-1
+b = a[:]
+b.sort()
+second=-1
 # Please write your code here.
 for i in range(1, n):
-    if a[i] > a[0]:
-        if a[i]<second_min:
-            second_min = a[i]
-            second_index=i+1
-    elif a[i] < a[0]:
-        second_index=-1
-        break
+    if b[i]>b[0] and second==-1:
+        second=b[i]
+    elif second==b[i]:
+        print(-1)
+        sys.exit()
 
-print(second_index)
+
+if second!=-1:
+    print(a.index(second)+1)
+else:
+    print(second)
