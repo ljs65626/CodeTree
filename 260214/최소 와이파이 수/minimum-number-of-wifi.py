bf4644d1a5c7defb85570ee2c932d44ce23224fb
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-02-14 18:19:11 +0900

    Title: 최소 와이파이 수, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260214/최소 와이파이 수/minimum-number-of-wifi.py
+++ 260214/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,13 +1,22 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-isit=[False]*n
-ans=0
+# isit=[False]*n
+# ans=0
 # Please write your code here.
-for i in range(n):
-    if arr[i]==1 and isit[i]==False:
-        for j in range(i, i+(2*m+1)):
-            if j>=n:
-                break
-            isit[j]=True
+# for i in range(n):
+#     if arr[i]==1 and isit[i]==False:
+#         for j in range(i, i+(2*m+1)):
+#             if j>=n:
+#                 break
+#             isit[j]=True
+#         ans+=1
+
+i=0
+ans=0
+while i<n:
+    if arr[i]==1:
         ans+=1
+        i += (2*m+1)
+    else:
+        i+=1
 print(ans)
