c1c439e866d22600381c4eab635c34fee20d2ff2
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-02-19 17:01:26 +0900

    Title: 전부 포함하는 선분, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

--- 260219/전부 포함하는 선분/all-inclusive-segment.py
+++ 260219/전부 포함하는 선분/all-inclusive-segment.py
@@ -5,11 +5,33 @@ segments = [list(map(int, input().split())) for _ in range(n)]
 # xone = list(xone)
 # xtwo = list(xtwo)
 # Please write your code here.
-xone = sorted(segments, key=lambda t: t[0])
-xtwo = sorted(segments, key=lambda t: t[1])
-if xone[0]==xtwo[n-1]:
-    print(xtwo[n-2][1]-xone[1][0])
-    sys.exit()
-ans = min(xtwo[n-1][1]-xone[1][0], xtwo[n-2][1]-xone[0][0])
+# xone = sorted(segments, key=lambda t: t[0])
+# xtwo = sorted(segments, key=lambda t: t[1])
+# if xone[0]==xtwo[n-1]:
+#     print(xtwo[n-2][1]-xone[1][0])
+#     sys.exit()
+# ans = min(xtwo[n-1][1]-xone[1][0], xtwo[n-2][1]-xone[0][0])
+# print(ans)
+
+
+
+ans = sys.maxsize
+for i in range(n):
+    line = [0]*101
+    for j in range(n):
+        if i==j:
+            continue
+        x1, x2 = segments[j]
+        for k in range(x1, x2+1):
+            line[k]=1
+    first_index=-1
+    for j in range(1, 101):
+        if first_index==-1 and line[j]==1:
+            first_index=j
+        elif line[j]==1:
+            last_index=j
+    
+    ans = min(last_index-first_index, ans)
+
 print(ans)
 
