5c5c76f405dd204d2a62310cdadbc353d5c0a5ee
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-02-19 16:47:58 +0900

    Title: 전부 포함하는 선분, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 260219/전부 포함하는 선분/all-inclusive-segment.py
+++ 260219/전부 포함하는 선분/all-inclusive-segment.py
@@ -1,10 +1,15 @@
+import sys
 n = int(input())
 segments = [list(map(int, input().split())) for _ in range(n)]
-xone, xtwo=zip(*segments)
-xone = list(xone)
-xtwo = list(xtwo)
+# xone, xtwo=zip(*segments)
+# xone = list(xone)
+# xtwo = list(xtwo)
 # Please write your code here.
-xone.sort()
-xtwo.sort()
-ans = min(xtwo[-1]-xone[1], xtwo[-2]-xone[0])
+xone = sorted(segments, key=lambda t: t[0])
+xtwo = sorted(segments, key=lambda t: t[1])
+if xone[0]==xtwo[n-1]:
+    print(xtwo[n-2][1]-xone[1][0])
+    sys.exit()
+ans = min(xtwo[n-1][1]-xone[1][0], xtwo[n-2][1]-xone[0][0])
 print(ans)
+
