420bc056e99a9a2ad803f54e661a5df8c0b16087
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-02-19 15:56:23 +0900

    Title: 두 점으로 만드는 직사각형, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260219/두 점으로 만드는 직사각형/rectangle-made-of-two-points.py
@@ -0,0 +1,11 @@
+x1, y1, x2, y2 = map(int, input().split())
+a1, b1, a2, b2 = map(int, input().split())
+
+# Please write your code here.
+maxx = max(x1, x2, a1, a2)
+minx = min(x1, x2, a1, a2)
+
+maxy = max(y1, y2, b1, b2)
+miny = min(y1, y2, b1, b2)
+
+print((maxx-minx)*(maxy-miny))
