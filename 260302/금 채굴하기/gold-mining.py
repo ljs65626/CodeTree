b81eb350ca99ea1e4da78cad4aef26dd478d64de
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-02 19:41:23 +0900

    Title: 금 채굴하기, Time: 69ms, Memory: 17MB, Status: Passed - Codetree

--- 260302/금 채굴하기/gold-mining.py
+++ 260302/금 채굴하기/gold-mining.py
@@ -4,31 +4,27 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 
 
-def maremmo(k, i, j, square):
-    nx = i-k  #leftupi
-    ny = j-k  #leftupj
+def maremmo(k, i, j):
     gold=0
-    for a in range(square):
-        for b in range(square):
-            if nx+a<0 or ny+b<0 or nx+a>=n or ny+b>=n:
-                continue
-            if (abs(i-(nx+a))+abs(j-(ny+b))) <= k:
-                gold+=grid[nx+a][ny+b]
+    for a in range(n):
+        for b in range(n):
+            # if nx+a<0 or ny+b<0 or nx+a>=n or ny+b>=n:
+            #     continue
+            if (abs(i-a)+abs(j-b)) <= k:
+                gold+=grid[a][b]
     
     return gold
 
 
-square=1
 ans=0
 for k in range(n+1):
     mining = (k**2) + ((k+1)**2)
     for i in range(n):
         for j in range(n):
-            gold_cnt = maremmo(k, i, j, square)
+            gold_cnt = maremmo(k, i, j)
             price = gold_cnt*m
             if price>=mining:
                 ans = max(ans, gold_cnt)
-    square+=2
 
 
 print(ans)
