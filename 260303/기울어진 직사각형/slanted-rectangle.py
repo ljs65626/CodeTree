888dc8af6c691cbb05cf38e0037586297196ca82
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-03 22:48:50 +0900

    Title: 기울어진 직사각형, Time: 133ms, Memory: 20MB, Status: Passed - Codetree

--- 260303/기울어진 직사각형/slanted-rectangle.py
+++ 260303/기울어진 직사각형/slanted-rectangle.py
@@ -16,38 +16,48 @@ def sum_of_num(i, j, w, h):
     nx=i
     ny=j
     sum_val=0
-    for k in range(w):
-        if not in_range(nx+dxs[0], ny+dys[0]):
-            return 0
-        else:
-            sum_val+=grid[nx+dxs[0]][ny+dys[0]]
-            nx+=dxs[0]
-            ny+=dys[0]
+    moves = [w, h, w, h]
+    # for k in range(w):
+    #     if not in_range(nx+dxs[0], ny+dys[0]):
+    #         return 0
+    #     else:
+    #         sum_val+=grid[nx+dxs[0]][ny+dys[0]]
+    #         nx+=dxs[0]
+    #         ny+=dys[0]
     
-    for k in range(h):
-        if not in_range(nx+dxs[1], ny+dys[1]):
-            return 0
-        else:
-            sum_val+=grid[nx+dxs[1]][ny+dys[1]]
-            nx+=dxs[1]
-            ny+=dys[1]
+    # for k in range(h):
+    #     if not in_range(nx+dxs[1], ny+dys[1]):
+    #         return 0
+    #     else:
+    #         sum_val+=grid[nx+dxs[1]][ny+dys[1]]
+    #         nx+=dxs[1]
+    #         ny+=dys[1]
 
-    for k in range(w):
-        if not in_range(nx+dxs[2], ny+dys[2]):
-            return 0
-        else:
-            sum_val+=grid[nx+dxs[2]][ny+dys[2]]
-            nx+=dxs[2]
-            ny+=dys[2]
+    # for k in range(w):
+    #     if not in_range(nx+dxs[2], ny+dys[2]):
+    #         return 0
+    #     else:
+    #         sum_val+=grid[nx+dxs[2]][ny+dys[2]]
+    #         nx+=dxs[2]
+    #         ny+=dys[2]
     
-    for k in range(h):
-        if not in_range(nx+dxs[3], ny+dys[3]):
-            return 0
-        else:
-            sum_val+=grid[nx+dxs[3]][ny+dys[3]]
-            nx+=dxs[3]
-            ny+=dys[3]
+    # for k in range(h):
+    #     if not in_range(nx+dxs[3], ny+dys[3]):
+    #         return 0
+    #     else:
+    #         sum_val+=grid[nx+dxs[3]][ny+dys[3]]
+    #         nx+=dxs[3]
+    #         ny+=dys[3]
     
+    # return sum_val
+
+    for dx, dy, move in zip(dxs, dys, moves):
+        for _ in range(move):
+            nx+=dx
+            ny+=dy
+            if not in_range(nx, ny):
+                return 0
+            sum_val+=grid[nx][ny]
     return sum_val
 
 ans=0
