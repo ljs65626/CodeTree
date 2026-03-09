b506fef91fd300784f662ebbb00c9232c6392f3e
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-09 14:38:55 +0900

    Title: 기울어진 직사각형의 회전, Time: 94ms, Memory: 18MB, Status: Passed - Codetree

--- 260309/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 260309/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -1,142 +1,35 @@
 import copy
-n = int(input())
+n=int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
-r, c, m1, m2, m3, m4, dir1 = map(int, input().split())
-
-# Please write your code here.
-
-# def move(x, y, m1, m2, m3, m4, direction):
-#     nx=x
-#     ny=y
-#     tmp = copy.deepcopy(grid)
-#     if direction==0:
-#         dx = [-1, -1, 1, 1]
-#         dy = [1, -1, -1, 1]
-#         dir2 = 0
-#         for i in range(m1):
-#             if nx+dx[dir2]<n and nx+dx[dir2]>=0 and ny+dy[dir2]<n and ny+dy[dir2]>=0:
-#                 pass
-#             else:
-#                 dir2=(dir2+1)%4
-#             nx+=dx[dir2]
-#             ny+=dy[dir2]
-#             tmp[nx][ny] = grid[nx-dx[dir2]][ny-dy[dir2]]
-#         dir2=(dir2+1)%4
-
-#         for i in range(m2):
-#             if nx+dx[dir2]<n and nx+dx[dir2]>=0 and ny+dy[dir2]<n and ny+dy[dir2]>=0:
-#                 pass
-#             else:
-#                 dir2=(dir2+1)%4
-#             nx+=dx[dir2]
-#             ny+=dy[dir2]
-#             tmp[nx][ny] = grid[nx-dx[dir2]][ny-dy[dir2]]
-#         dir2=(dir2+1)%4
-        
-#         for i in range(m3):
-#             if nx+dx[dir2]<n and nx+dx[dir2]>=0 and ny+dy[dir2]<n and ny+dy[dir2]>=0:
-#                 pass
-#             else:
-#                 dir2=(dir2+1)%4
-#             nx+=dx[dir2]
-#             ny+=dy[dir2]
-#             tmp[nx][ny] = grid[nx-dx[dir2]][ny-dy[dir2]]
-#         dir2=(dir2+1)%4
-        
-#         for i in range(m4):
-#             if nx+dx[dir2]<n and nx+dx[dir2]>=0 and ny+dy[dir2]<n and ny+dy[dir2]>=0:
-#                 pass
-#             else:
-#                 dir2=(dir2+1)%4
-#             nx+=dx[dir2]
-#             ny+=dy[dir2]
-#             tmp[nx][ny] = grid[nx-dx[dir2]][ny-dy[dir2]]
-        
-        
-#         tmp[x][y] = grid[nx-dx[dir2]][ny-dy[dir2]]
-#     else:
-#         dx = [-1, -1, 1, 1]
-#         dy = [-1, 1, 1, -1]
-#         dir2 = 0
-#         origi = dir2
-#         for i in range(m1):
-#             if nx+dx[dir2]<n and nx+dx[dir2]>=0 and ny+dy[dir2]<n and ny+dy[dir2]>=0:
-#                 pass
-#             else:
-#                 dir2=(dir2+1)%4
-#             nx+=dx[dir2]
-#             ny+=dy[dir2]
-#             tmp[nx][ny] = grid[nx-dx[dir2]][ny-dy[dir2]]
-#         dir2=(origi+1)%4
-#         origi = dir2
-#         for i in range(m2):
-#             if nx+dx[dir2]<n and nx+dx[dir2]>=0 and ny+dy[dir2]<n and ny+dy[dir2]>=0:
-#                 pass
-#             else:
-#                 dir2=(dir2+1)%4
-#             nx+=dx[dir2]
-#             ny+=dy[dir2]
-#             tmp[nx][ny] = grid[nx-dx[dir2]][ny-dy[dir2]]
-#         dir2=(origi+1)%4
-#         origi = dir2
-#         for i in range(m3):
-#             if nx+dx[dir2]<n and nx+dx[dir2]>=0 and ny+dy[dir2]<n and ny+dy[dir2]>=0:
-#                 pass
-#             else:
-#                 dir2=(dir2+1)%4
-#             nx+=dx[dir2]
-#             ny+=dy[dir2]
-#             tmp[nx][ny] = grid[nx-dx[dir2]][ny-dy[dir2]]
-#         dir2=(origi+1)%4
-#         origi=dir2
-#         for i in range(m4):
-#             if nx+dx[dir2]<n and nx+dx[dir2]>=0 and ny+dy[dir2]<n and ny+dy[dir2]>=0:
-#                 pass
-#             else:
-#                 dir2=(dir2+1)%4
-#             nx+=dx[dir2]
-#             ny+=dy[dir2]
-#             tmp[nx][ny] = grid[nx-dx[dir2]][ny-dy[dir2]]
-        
-#         tmp[x][y] = grid[nx-dx[dir2]][ny-dy[dir2]]
-
-#     return tmp
-
-import copy
+r, c, m1, m2, m3, m4, dir = map(int, input().split())
 
 def move(x, y, m1, m2, m3, m4, direction):
-    tmp = copy.deepcopy(grid)
-    
+    coords=[]
     dx = [-1, -1, 1, 1]
     dy = [1, -1, -1, 1]
-    move_counts = [m1, m2, m3, m4]
-    
-    coords = []
-    nx, ny = x, y
+    mm = [m1, m2, m3, m4]
+    dir2=0
     for d in range(4):
-        for _ in range(move_counts[d]):
-            nx += dx[d]
-            ny += dy[d]
-            coords.append((nx, ny))
-            
-    values = [grid[r][c] for r, c in coords]
+        for i in range(mm[d]):
+            x+=dx[dir2]
+            y+=dy[dir2]
+            coords.append((x, y))
+        dir2 = (dir2+1)%4
     
-    if direction == 0:
+    values = [grid[x1][y1] for x1, y1 in coords]
+
+    if direction==0:
         values = [values[-1]] + values[:-1]
     else:
         values = values[1:] + [values[0]]
-        
-    for i, (r, c) in enumerate(coords):
-        tmp[r][c] = values[i]
-        
-    return tmp
+    
+    for i, (x1, y1) in enumerate(coords):
+        grid[x1][y1] = values[i]
 
-tmp = move(r-1, c-1, m1, m2, m3, m4, dir1)
 
-for i in tmp:
+move(r-1, c-1, m1, m2, m3, m4, dir)
+
+for i in grid:
     for j in i:
         print(j, end=' ')
     print()
-        
-
-
