6961e5bb3cd5a6bbae16621e6b6eaaa6cfb0c8f7
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-11 16:26:45 +0900

    Title: 1차원 폭발 게임, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260311/1차원 폭발 게임/The-1D-bomb-game.py
+++ 260311/1차원 폭발 게임/The-1D-bomb-game.py
@@ -4,25 +4,47 @@ num = [int(input()) for _ in range(n)]
 
 # Please write your code here.
 while True:
-    if n<=1:
-        break
+    # if n<=1:
+    #     break
+    # tmp=[]
+    # for i in range(n):
+    #     if i==0:
+    #         if num[i]!=num[i+1]:
+    #             tmp.append(num[i])
+    #     elif i==n-1:
+    #         if num[i]!=num[i-1]:
+    #             tmp.append(num[i])
+    #     else:
+    #         if num[i]!=num[i+1] and num[i]!=num[i-1]:
+    #             tmp.append(num[i])
+    # if n==len(tmp):
+    #     break
+    # num = copy.deepcopy(tmp)
+    # n = len(num)
     tmp=[]
-    for i in range(n):
-        if i==0:
-            if num[i]!=num[i+1]:
-                tmp.append(num[i])
-        elif i==n-1:
-            if num[i]!=num[i-1]:
-                tmp.append(num[i])
+    before=num[0]
+    ccnt=1
+    for i in range(1, n):
+        if before==num[i]:
+            ccnt+=1
         else:
-            if num[i]!=num[i+1] and num[i]!=num[i-1]:
+            if ccnt<m:
+                tmp.append(before)
+            ccnt=1
+        if i==n-1:
+            if ccnt<m:
                 tmp.append(num[i])
-    if n==len(tmp):
+        before=num[i]
+    if n==len(tmp) or n==1:
         break
     num = copy.deepcopy(tmp)
     n = len(num)
+    if n==0:
+        break
 
 print(len(num))
 for i in range(n):
     print(num[i])
+
+
 
