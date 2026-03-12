c80f7cedc0a4ceec5b1509b07823e58e1f93a2b7
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-12 16:50:02 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 238ms, Memory: 27MB, Status: Passed - Codetree

---
+++ 260312/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
@@ -0,0 +1,20 @@
+K, N = map(int, input().split())
+answer=[]
+# Please write your code here.
+
+def print_answer():
+    for elem in answer:
+        print(elem, end=' ')
+    print()
+
+def choose(curr_num):
+    if curr_num==N+1:
+        print_answer()
+        return
+    
+    for i in range(1, K+1):
+        answer.append(i)
+        choose(curr_num+1)
+        answer.pop()
+
+choose(1)
