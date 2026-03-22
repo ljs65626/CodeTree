eb7bbe9935a95c5375eec698c0fb031e4f5bfea9
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-22 22:01:40 +0900

    Title: N개 중에 M개 뽑기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260322/N개 중에 M개 뽑기/n-choose-m.py
@@ -0,0 +1,21 @@
+N, M = map(int, input().split())
+arr=[]
+# Please write your code here.
+def print_answer():
+    for i in arr:
+        print(i, end=' ')
+    print()
+
+
+def backtrack(curr_num, cnt):
+    if curr_num==M+1:
+        print_answer()
+        return
+    
+    for i in range(cnt, N+1):
+        arr.append(i)
+        backtrack(curr_num+1, i+1)
+        arr.pop()
+
+
+backtrack(1, 1)
