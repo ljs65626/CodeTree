5c8a036f24aa1a75de32ce3adbbbb47b49bb7c22
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-24 18:42:44 +0900

    Title: N개의 점 중 M개 고르기, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260324/N개의 점 중 M개 고르기/choose-m-out-of-n-points.py
@@ -0,0 +1,37 @@
+import sys
+n, m = map(int, input().split())
+coord = list(tuple(map(int, input().split())) for _ in range(n))
+arr=[]
+ans = sys.maxsize
+# Please write your code here.
+def dist(x1, y1, x2, y2):
+    return pow(((x1-x2)**2) + ((y1-y2)**2), 1/2)
+
+def func():
+    max_dist = -sys.maxsize
+    for i in range(m):
+        x1, y1 = arr[i]
+        for j in range(i+1, m):
+            x2, y2 = arr[j]
+            max_dist = max(dist(x1, y1, x2, y2), max_dist)
+    return max_dist
+
+
+def backtrack(curr_loc, cnt):
+    global ans
+    if curr_loc==n:
+        if cnt==m:
+            ans = min(ans, func())
+        return
+    
+    for i in range(1, -1, -1):
+        if i==1:
+            arr.append(coord[curr_loc])
+            backtrack(curr_loc+1, cnt+1)
+            arr.pop()
+        else:
+            backtrack(curr_loc+1, cnt)
+
+
+backtrack(0, 0)
+print(int(ans**2))
