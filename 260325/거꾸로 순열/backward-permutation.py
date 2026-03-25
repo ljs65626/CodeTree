f9a00aaf34282cf74392c5888f216b9f1d77dc53
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-25 18:04:15 +0900

    Title: 거꾸로 순열, Time: 182ms, Memory: 25MB, Status: Passed - Codetree

---
+++ 260325/거꾸로 순열/backward-permutation.py
@@ -0,0 +1,25 @@
+n = int(input())
+arr=[]
+visited=[False]*(n+1)
+# Please write your code here.
+def print_answer():
+    for i in arr:
+        print(i, end=' ')
+    print()
+
+def backtrack(curr_loc):
+    if curr_loc==n+1:
+        print_answer()
+        return
+    
+    for i in range(n, 0, -1):
+        if visited[i]==True:
+            continue
+        
+        visited[i]=True
+        arr.append(i)
+        backtrack(curr_loc+1)
+        arr.pop()
+        visited[i]=False
+
+backtrack(1)
