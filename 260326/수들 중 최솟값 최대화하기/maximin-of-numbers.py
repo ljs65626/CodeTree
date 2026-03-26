8cb8e89598454bfc27c89ddaf92fe1c465739acc
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-26 21:43:37 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260326/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,24 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+ans=-1
+visited=[False]*n
+# Please write your code here.
+
+def backtrack(curr_loc, minnest):
+    global ans
+    if curr_loc==n:
+        ans = max(minnest, ans)
+        return
+
+    for i in range(n):
+        if visited[i]==True:
+            continue
+        
+        
+        visited[i]=True
+        backtrack(curr_loc+1, min(grid[curr_loc][i], minnest))
+        visited[i]=False
+    
+
+backtrack(0, 10001)
+print(ans)
