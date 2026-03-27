41d493b63b086cdd6ad7ad61eb5bf93f06622709
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-27 18:09:54 +0900

    Title: 그래프 탐색, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260327/그래프 탐색/graph-traversal.py
+++ 260327/그래프 탐색/graph-traversal.py
@@ -4,37 +4,40 @@ ans=0
 visited = [False] * (n+1)
 visited[1]=[True]
 # Please write your code here.
-# graph = [[] for _ in range(n+1)]
-# for a, b in edge:
-#     graph[a].append(b)
-#     graph[b].append(a)
 
-# # print(graph)
-
-# def dfs1(vertex):
-#     global ans
-#     for curr_v in graph[vertex]:
-#         if not visited[curr_v]:
-#             visited[curr_v]=True
-#             ans+=1
-#             dfs1(curr_v)
-
-# dfs1(1)
-# print(ans)
-
-
-graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
+## dfs1(인접리스트 방식) 
+graph = [[] for _ in range(n+1)]
 for a, b in edge:
-    graph[a][b]=1
-    graph[b][a]=1
+    graph[a].append(b)
+    graph[b].append(a)
+
 # print(graph)
-def dfs2(vertex):
+
+def dfs1(vertex):
     global ans
-    for curr_v in range(1, n+1):
-        if not visited[curr_v] and graph[vertex][curr_v]==1:
-            visited[curr_v]=1
+    for curr_v in graph[vertex]:
+        if not visited[curr_v]:
+            visited[curr_v]=True
             ans+=1
-            dfs2(curr_v)
-dfs2(1)
+            dfs1(curr_v)
+
+dfs1(1)
 print(ans)
+
+
+## dfs2(인접행렬 방식)
+# graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
+# for a, b in edge:
+#     graph[a][b]=1
+#     graph[b][a]=1
+# # print(graph)
+# def dfs2(vertex):
+#     global ans
+#     for curr_v in range(1, n+1):
+#         if not visited[curr_v] and graph[vertex][curr_v]==1:
+#             visited[curr_v]=1
+#             ans+=1
+#             dfs2(curr_v)
+# dfs2(1)
+# print(ans)
 
