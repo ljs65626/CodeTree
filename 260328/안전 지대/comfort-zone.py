b74d49836eab824f83aebfcd865fea6c32845f50
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-28 18:47:12 +0900

    Title: 안전 지대, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260328/안전 지대/comfort-zone.py
+++ 260328/안전 지대/comfort-zone.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10**5)
+sys.setrecursionlimit(2500)
 
 n, m = tuple(map(int, input().split()))
 grid = [list(map(int, input().split())) for _ in range(n)]
@@ -7,11 +7,11 @@ visited = [[False for _ in range(m)] for _ in range(n)]
 dxs = [1, 0, -1, 0]
 dys = [0, 1, 0, -1]
 # Please write your code here.
-def swim(k):
-    for i in range(n):
-        for j in range(m):
-            if grid[i][j]<=k:
-                grid[i][j]=0
+# def swim(k):
+#     for i in range(n):
+#         for j in range(m):
+#             if grid[i][j]<=k:
+#                 grid[i][j]=0
 
 def in_range(x, y):
     if x>=0 and x<n and y>=0 and y<m:
@@ -19,22 +19,22 @@ def in_range(x, y):
     else:
         return False
 
-def can_go(x, y):
+def can_go(x, y, k):
     if not in_range(x, y):
         return False
     if visited[x][y]==True:
         return False
-    if grid[x][y]==0:
+    if grid[x][y]<=k:
         return False
     return True
 
-def dfs(x, y):
+def dfs(x, y, k):
     for dx, dy in zip(dxs, dys):
         new_x = x+dx
         new_y = y+dy
-        if can_go(new_x, new_y):
+        if can_go(new_x, new_y, k):
             visited[new_x][new_y]=True
-            dfs(new_x, new_y)
+            dfs(new_x, new_y, k)
 
 def reset():
     for i in range(n):
@@ -46,15 +46,14 @@ max_val = max(max(grid))
 ans_k=1
 ans_cnt=0
 for k in range(1, max_val+1):
-    swim(k)
     reset()
     # print(visited)
     safe_cnt=0
     for i in range(n):
         for j in range(m):
-            if grid[i][j]>0 and visited[i][j]==False:
+            if grid[i][j]>k and visited[i][j]==False:
                 visited[i][j]=True
-                dfs(i, j)
+                dfs(i, j, k)
                 safe_cnt+=1
     if safe_cnt>ans_cnt:
         ans_k=k
