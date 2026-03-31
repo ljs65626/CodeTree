0fd9ca617730b0d06819cbd1caa0b8a3ba8f79af
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-03-31 19:03:28 +0900

    Title: 떨어지는 1자 블록, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260331/떨어지는 1자 블록/falling-horizontal-block.py
+++ 260331/떨어지는 1자 블록/falling-horizontal-block.py
@@ -3,20 +3,21 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 # k: 블록이 떨어질 열(column) 위치
 # Please write your code here.
 k-=1
-tmp_row=-1
-for row in range(n-1, -1, -1):
+max_row=-1
+for row in range(n):
     is_satisfied=True
     for col in range(k, k+m):
         if grid[row][col]==1:
             is_satisfied=False
             break
     if is_satisfied:
-        tmp_row = row
+        max_row = row
+    else:
         break
 
-if is_satisfied:
+if max_row!=-1:
     for col in range(k, k+m):
-        grid[tmp_row][col] = 1
+        grid[max_row][col] = 1
 
 for i in range(n):
     for j in range(n):
