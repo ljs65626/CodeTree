b5a7430fa7e28f4376c58c99c73cecbd573bcb35
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-01 22:16:20 +0900

    Title: 수 등장 횟수, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 260401/수 등장 횟수/number-frequency.py
+++ 260401/수 등장 횟수/number-frequency.py
@@ -2,17 +2,20 @@ n, m = map(int, input().split())
 n_arr = list(map(int, input().split()))
 m_arr = list(map(int, input().split()))
 
-d= dict()
+# d= dict()
 
-for i in n_arr:
-    if i not in d:
-        d[i] = 1
-    else:
-        d[i] += 1
+# for i in n_arr:
+#     if i not in d:
+#         d[i] = 1
+#     else:
+#         d[i] += 1
 
-# print(d)
+# # print(d)
+# for i in m_arr:
+#     if i in d:
+#         print(d[i], end=' ')
+#     else:
+#         print(0, end=' ')
+
 for i in m_arr:
-    if i in d:
-        print(d[i], end=' ')
-    else:
-        print(0, end=' ') 
+    print(n_arr.count(i), end=' ')
