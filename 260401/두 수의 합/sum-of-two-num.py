0a110ffa9a0e2fb904cf6b59350ddef7a4cd35bb
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-01 23:45:29 +0900

    Title: 두 수의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260401/두 수의 합/sum-of-two-num.py
+++ 260401/두 수의 합/sum-of-two-num.py
@@ -1,16 +1,18 @@
 n, k = map(int, input().split())
-d = {}
 arr = list(map(int, input().split()))
-for i in arr:
-    if i in d:
-        d[i] +=1
-    else:
-        d[i] = 1
 
-ans=0
-for key in d:
-    if (k-key in d) and d[key]>0 and d[k-key]>0:
-        d[key]-=1
-        d[k-key]-=1
-        ans+=1
+d = {}
+ans = 0
+
+for num in arr:
+    diff = k - num
+    
+    if diff in d:
+        ans += d[diff]
+    
+    if num in d:
+        d[num] += 1
+    else:
+        d[num] = 1
+
 print(ans)
