724df667088275cf7586b524ab25e8f72b2a4937
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-01 22:17:18 +0900

    Title: 수 등장 횟수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260401/수 등장 횟수/number-frequency.py
+++ 260401/수 등장 횟수/number-frequency.py
@@ -2,20 +2,22 @@ n, m = map(int, input().split())
 n_arr = list(map(int, input().split()))
 m_arr = list(map(int, input().split()))
 
-# d= dict()
+d= dict()
 
-# for i in n_arr:
-#     if i not in d:
-#         d[i] = 1
-#     else:
-#         d[i] += 1
+for i in n_arr:
+    if i not in d:
+        d[i] = 1
+    else:
+        d[i] += 1
 
-# # print(d)
-# for i in m_arr:
-#     if i in d:
-#         print(d[i], end=' ')
-#     else:
-#         print(0, end=' ')
-
+# print(d)
 for i in m_arr:
-    print(n_arr.count(i), end=' ')
+    if i in d:
+        print(d[i], end=' ')
+    else:
+        print(0, end=' ')
+
+
+#count() 함수는 O(N)이기 때문에 결국 O(N^2)으로 시간 초과이다.
+# for i in m_arr:
+#     print(n_arr.count(i), end=' ')
