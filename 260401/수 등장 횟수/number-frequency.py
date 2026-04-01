eb99ceec8a44f105f3f1de3272bdb16f995448df
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-01 22:14:28 +0900

    Title: 수 등장 횟수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260401/수 등장 횟수/number-frequency.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+n_arr = list(map(int, input().split()))
+m_arr = list(map(int, input().split()))
+
+d= dict()
+
+for i in n_arr:
+    if i not in d:
+        d[i] = 1
+    else:
+        d[i] += 1
+
+# print(d)
+for i in m_arr:
+    if i in d:
+        print(d[i], end=' ')
+    else:
+        print(0, end=' ') 
