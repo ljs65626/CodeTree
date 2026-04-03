f2660dc92aec4d5441333b9ca5735023aeb18ed7
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-03 20:09:16 +0900

    Title: 사각형 채우기 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260403/사각형 채우기 3/rectangle-fill-3.py
+++ 260403/사각형 채우기 3/rectangle-fill-3.py
@@ -2,11 +2,10 @@ n = int(input())
 
 # Please write your code here.
 dp = [0] * 1001
-
+dp[0] = 1
 dp[1] = 2
 dp[2] = 7
-
 for i in range(3, n+1):
-    dp[i] = ((dp[i-1]*2) + (dp[i-2]*4)) % 1_000_000_007
+    dp[i] = (3*dp[i-1] + dp[i-2] - dp[i-3]) % 1_000_000_007
 
 print(dp[n])
