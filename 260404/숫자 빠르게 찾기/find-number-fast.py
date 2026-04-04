5dff84c6af90e39c29f6642b9d89bfc8d5dd732c
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-04 15:13:03 +0900

    Title: 숫자 빠르게 찾기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260404/숫자 빠르게 찾기/find-number-fast.py
+++ 260404/숫자 빠르게 찾기/find-number-fast.py
@@ -1,13 +1,31 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-d={}
-# Please write your code here.
-for i in range(n):
-    d[arr[i]] = i+1
+# d={}
+# # Please write your code here.
+# for i in range(n):
+#     d[arr[i]] = i+1
+
+def binary_search(target):
+    left = 0
+    right = len(arr)-1
+    while left <= right:
+        mid = (left+right) //2
+        if arr[mid]==target:
+            return mid
+        elif arr[mid]>target:
+            right = mid-1
+        else:
+            left = mid+1
+    return -1
 
 for _ in range(m):
     q = int(input())
-    if q in d:
-        print(d[q])
+    # if q in d:
+    #     print(d[q])
+    # else:
+    #     print(-1)
+    ret_val = binary_search(q)
+    if ret_val!=-1:
+        print(ret_val+1)
     else:
-        print(-1)
+        print(ret_val)
