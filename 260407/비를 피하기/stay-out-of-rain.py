65b17fbf7bc92deb728025e1a1c9e5c748afff0b
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-07 21:53:39 +0900

    Title: 비를 피하기, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 260407/비를 피하기/stay-out-of-rain.py
@@ -0,0 +1,72 @@
+from collections import deque
+import sys
+n, h, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False for _ in range(n)] for _ in range(n)]
+q = deque()
+ans = [[0 for _ in range(n)] for _ in range(n)]
+score = [[0 for _ in range(n)] for _ in range(n)]
+dxs = [-1, 1, 0, 0]
+dys = [0, 0, -1, 1]
+
+def check():
+    min_val = sys.maxsize
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j]==3 and visited[i][j]==True:
+                min_val = min(score[i][j], min_val)
+    
+    if min_val==sys.maxsize:
+        return -1
+    else:
+        return min_val
+
+def push(x, y, v):
+    visited[x][y] = True
+    q.append((x, y))
+    score[x][y] = v
+
+def in_range(x, y):
+    if x>=0 and x<n and y>=0 and y<n:
+        return True
+    else:
+        return False
+
+def can_go(x, y):
+    if not in_range(x, y):
+        return False
+    if visited[x][y]==True:
+        return False
+    if grid[x][y]==1:
+        return False
+    return True
+
+def reset():
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = False
+            score[i][j] = 0
+
+def bfs():
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            new_x = x+dx
+            new_y = y+dy
+            if can_go(new_x, new_y):
+                push(new_x, new_y, score[x][y]+1)
+
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j]==2:
+            reset()
+            push(i, j, 0)
+            bfs()
+            ans[i][j] = check()
+
+
+for i in range(n):
+    for j in range(n):
+        print(ans[i][j], end=' ')
+    print()
