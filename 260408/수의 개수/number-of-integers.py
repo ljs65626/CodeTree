49f883a31728b1003247a8b49e7f9c9a80890316
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-08 18:39:42 +0900

    Title: 수의 개수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/수의 개수/number-of-integers.py
+++ 260408/수의 개수/number-of-integers.py
@@ -7,7 +7,7 @@ arr = list(map(int, input().split()))
 def upper_bound(target):
     left = 0
     right = n-1
-    min_idx = sys.maxsize
+    min_idx = n
     while left<=right:
         mid = (left+right)//2
         if arr[mid]>target:
@@ -15,15 +15,13 @@ def upper_bound(target):
             min_idx = min(min_idx, mid)
         else:
             left = mid+1
-    if min_idx==sys.maxsize:
-        return 0
-    else:
-        return min_idx
+
+    return min_idx
 
 def lower_bound(target):
     left = 0
     right = n-1
-    min_idx = sys.maxsize
+    min_idx = n
     while left<=right:
         mid = (left+right)//2
         if arr[mid]>=target:
@@ -31,16 +29,11 @@ def lower_bound(target):
             min_idx = min(min_idx, mid)
         else:
             left = mid+1
-    if min_idx==sys.maxsize:
-        return 0
-    else:
-        return min_idx
+
+    return min_idx
 
 for _ in range(m):
     inp = int(input())
     upper = upper_bound(inp)
     lower = lower_bound(inp)
-    if upper-lower<0:
-        print(1)
-    else:
-        print(upper-lower)
+    print(upper-lower)
