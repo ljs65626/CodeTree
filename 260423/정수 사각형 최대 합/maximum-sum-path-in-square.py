4ffb51a694b1917d758de980fb763ce8e861f281
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-23 18:11:53 +0900

    Title: 정수 사각형 최대 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260423/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -0,0 +1,22 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0 for _ in range(n)] for _ in range(n)]
+# Please write your code here.
+
+def initialize():
+    dp[0][0] = grid[0][0]
+    for i in range(1, n):
+        dp[i][0] = dp[i-1][0] + grid[i][0]
+
+    for i in range(1, n):
+        dp[0][i] = dp[0][i-1] + grid[0][i]
+
+
+initialize()
+
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(dp[i][j-1]+grid[i][j], dp[i-1][j]+grid[i][j])
+
+print(dp[n-1][n-1])
