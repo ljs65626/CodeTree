289c5faedc7b976f88aff8ba89a3afcd1ea181df
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-24 22:28:57 +0900

    Title: 회의실 준비 구현, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260424/회의실 준비 구현/implement-scheduling-meeting-room.py
@@ -0,0 +1,18 @@
+n = int(input())
+meetings = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+meetings.sort(key=lambda t : t[1])
+# print(meetings)
+ans=1
+before = 0
+for i in range(1, n):
+    start, end = meetings[i]
+    b_start, b_end = meetings[before]
+    if start<b_end:
+        continue
+    else:
+        ans+=1
+        before = i
+
+print(ans)
