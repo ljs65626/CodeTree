bd9a63d3b6c84ddd4a46aaf837d9cded31e8b6e7
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-25 19:15:41 +0900

    Title: 수 채우기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260425/수 채우기/fill-in-number.py
+++ 260425/수 채우기/fill-in-number.py
@@ -2,14 +2,11 @@ import sys
 n = int(input())
 cnt=0
 # Please write your code here.
-if n==5:
-    print(1)
-    sys.exit()
-if n==2:
-    print(1)
-    sys.exit()
 
 while n>0:
+    if n==5:
+        cnt+=1
+        break
     if n-5>=2:
         cnt+=1
         n-=5
