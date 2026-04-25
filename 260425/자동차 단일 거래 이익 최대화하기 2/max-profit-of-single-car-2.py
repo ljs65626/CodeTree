f1b48b3240e279f72ee7a887156cd3c7e75bc003
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-04-25 22:09:33 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260425/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 260425/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -4,12 +4,17 @@ arr = list(map(int, input().split()))
 # Please write your code here.
 ans = 0
 first = 0
+before_first=0
 for i in range(1, n):
     if arr[i]<arr[i-1]:
         diff=0
+        if arr[before_first] > arr[first]:
+            before_first = first
         first = i 
     else:
         end = i
+        if arr[before_first] < arr[first]:
+            first = before_first
         diff = arr[end] - arr[first]
         ans = max(diff, ans)
     
