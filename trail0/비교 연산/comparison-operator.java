98c0cb984c7e3f77c4ad88ffeb43513328c96bb8
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-05-28 23:24:38 +0900

    [Passed] 비교 연산 (134ms, 10MB)

---
+++ trail0/비교 연산/comparison-operator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        if (a>=b)
+            System.out.println(1);
+        else
+            System.out.println(0);
+        
+        if (a>b)
+            System.out.println(1);
+        else
+            System.out.println(0);
+        
+        if (b>=a)
+            System.out.println(1);
+        else
+            System.out.println(0);
+        
+        if (b>a)
+            System.out.println(1);
+        else
+            System.out.println(0);
+
+        if (a==b)
+            System.out.println(1);
+        else
+            System.out.println(0);
+
+        if (a!=b)
+            System.out.println(1);
+        else
+            System.out.println(0);
+
+    }
+}
