964430fea7b6c14aa5d5ab4637f0921a82322924
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-06-06 17:15:08 +0900

    [Passed] 특정 조건 두 정수 비교 (132ms, 10MB)

---
+++ trail0/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        if (a<b) System.out.print(1 + " ");
+        else System.out.print(0 + " ");
+
+        if (a==b) System.out.print(1);
+        else System.out.print(0);
+    }
+}
