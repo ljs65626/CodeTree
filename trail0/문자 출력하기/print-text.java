ec12e974d4388e3faa294d8401a753ed22cfd480
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-06-06 17:40:30 +0900

    [Passed] 문자 출력하기 (127ms, 10MB)

--- trail0/문자 출력하기/print-text.java
+++ trail0/문자 출력하기/print-text.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
+        char a = sc.next().charAt(0);
         for (int i = 0; i<8; i++) System.out.print(a);
     }
 }
