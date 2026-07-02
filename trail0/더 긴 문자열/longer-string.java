8e03da3ad950656abf8e6c34c485b4df6ce85a9f
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-07-02 19:59:11 +0900

    [Passed] 더 긴 문자열 (121ms, 10MB)

---
+++ trail0/더 긴 문자열/longer-string.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        if (a.length() > b.length())
+            System.out.print(a + " " + a.length());
+        else if (b.length() > a.length())
+            System.out.print(b + " " + b.length());
+        else
+            System.out.print("same");
+    }
+}
