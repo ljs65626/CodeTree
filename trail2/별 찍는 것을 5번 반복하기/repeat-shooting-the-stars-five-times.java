e4c7c33ef2dc8d07091f04ffc2ea42656f486d70
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-07-03 19:37:53 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (95ms, 9MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void print10star() {
+        for (int i=0; i<10; i++) System.out.print("*");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Please write your code here.
+        for (int i=0; i<5; i++) print10star();
+    }
+}
