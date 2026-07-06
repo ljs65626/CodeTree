d62078ce60e84acf0d82b0e662dd34dd73017576
Lim Jin Seob <158082662+ljs65626@users.noreply.github.com>
2026-07-06 20:13:43 +0900

    [Passed] 1이 3개 이상 있는 위치 (262ms, 18MB)

--- trail2/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ trail2/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 public class Main {
-    public static boolean in_range(int x, int y, int n) {
+    public static int n;
+
+    public static int[] dxs = new int[]{1, 0, -1, 0};
+    public static int[] dys = new int[]{0, 1, 0, -1};
+
+    public static boolean in_range(int x, int y) {
         if (x>=0 && x<n && y>=0 && y<n) return true;
         else return false;
     }
@@ -9,7 +14,7 @@ public class Main {
         // Please write your code here.
         int ans=0;
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
         int[][] arr = new int[n][n];
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
@@ -17,8 +22,6 @@ public class Main {
             }
         }
 
-        int[] dxs = new int[]{1, 0, -1, 0};
-        int[] dys = new int[]{0, 1, 0, -1};
         
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
@@ -26,7 +29,7 @@ public class Main {
                 int ny=j;
                 int cnt=0;
                 for (int n_dir=0; n_dir<4; n_dir++) {
-                    if (in_range(nx+dxs[n_dir], ny+dys[n_dir], n) && arr[nx+dxs[n_dir]][ny+dys[n_dir]]==1) {
+                    if (in_range(nx+dxs[n_dir], ny+dys[n_dir]) && arr[nx+dxs[n_dir]][ny+dys[n_dir]]==1) {
                         cnt+=1;
                     }
                 }
